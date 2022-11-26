32bb12ca87541e9ef60be5efe0c6ddcda37cdbb6
utkarsh-pc <itsutkarsh123@gmail.com>
2022-11-27 02:56:53 +0530

    improved the MainPage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import YearGrid from "./components/YearGrid";
-
+import MainPage from "./components/MainPage";
+import YearPage from "./components/YearPage";
 function App() {
-  return (
-    <div className="w-screen h-screen">
-      <Navbar />
-      <Profile />
-      <YearGrid/>
-    </div>
-  );
+	return (
+		<div className="font-primary w-screen h-screen">
+			<MainPage />
+		</div>
+	);
 }
 
 export default App;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,22 +1,61 @@
+import me from "../assets/ME.jpg";
+import github from "../assets/github.svg";
+import linkedin from "../assets/linkedin.svg";
+import twitter from "../assets/twitter.svg";
+import share from "../assets/share.svg";
+import mail from "../assets/mail.svg";
+
 const Profile = () => {
-  return (
-    <div className="flex flex-col p-4 bg-gray-800 text-white gap-6">
-      <div className="w-28 h-28 bg-blue-600">
-        <img src="" alt="" />
-      </div>
-      <div>
-        <h2 className="text-4xl font-bold">Utkarsh Maurya</h2>
-        <p className="text-sm">CS 3rd year</p>
-        <p className="">I am a self-taught web developer with a strong eye for innovative design and a keen understanding of techniques geared toward optimum user experience.</p>
-      </div>
-      <div className="flex gap-2 mt-4">
-        <a href="www.github.com/utkarsh1311">Github</a>
-        <a href="www.github.com/utkarsh1311">LinkedIn</a>
-        <a href="www.github.com/utkarsh1311">Twitter</a>
-        <a href="www.github.com/utkarsh1311">Mail</a>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="flex flex-col p-4 bg-gray-800 text-white gap-6 relative">
+			<img
+				src={share}
+				className="absolute right-5 top-5"
+				alt="share icon"
+			/>
+			<div className="w-28 h-28 bg-blue-600">
+				<img
+					src={me}
+					alt=""
+				/>
+			</div>
+			<div>
+				<h2 className="text-4xl font-bold">Utkarsh Maurya</h2>
+				<p className="text-sm">CS 3rd year</p>
+				<p className="leading-tight opacity-80 mt-2">
+					I am a self-taught web developer with a strong eye for innovative
+					design and a keen understanding of techniques geared toward optimum
+					user experience.
+				</p>
+			</div>
+			<div className="flex gap-4 mt-2">
+				<a href="www.github.com/utkarsh1311">
+					<img
+						src={github}
+						alt=""
+					/>
+				</a>
+				<a href="https://linkedin.com/utkarsh-mam">
+					<img
+						src={linkedin}
+						alt="linkedin icon"
+					/>
+				</a>
+				<a href="www.github.com/utkarsh1311">
+					<img
+						src={twitter}
+						alt="twitter icon"
+					/>
+				</a>
+				<a href="www.github.com/utkarsh1311">
+					<img
+						src={mail}
+						alt="mail icon"
+					/>
+				</a>
+			</div>
+		</div>
+	);
+};
 
 export default Profile;
