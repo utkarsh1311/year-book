099e1587ca17a9d03fcee02bdaf6eb6b6d7ff30d
utkarsh-pc <itsutkarsh123@gmail.com>
2022-11-27 03:30:04 +0530

    fixed sizing issue

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,11 +3,11 @@ import Profile from "./Profile";
 import YearGrid from "./YearGrid";
 const MainPage = () => {
 	return (
-		<>
+		<div className="h-screen bg-gray-800">
 			<Navbar />
 			<Profile />
 			<YearGrid />
-		</>
+		</div>
 	);
 };
 
