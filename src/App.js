02d7d44d196333eccf0613b269147f0864162eb3
utkarsh-pc <itsutkarsh123@gmail.com>
2023-02-06 18:14:46 +0530

    feat:add Login page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@
 // import Profile from "./components/Profile";
 // import YearGrid from "./components/YearGrid";
 import MainPage from "./components/MainPage";
-import YearPage from "./components/YearPage";
+import Login from './components/Login';
+// import YearPage from "./components/YearPage";
 function App() {
 	return (
 		<div className="font-primary bg-gray-900 w-screen min-h-screen md:px-16 lg:grid lg:grid-cols-5 ">
 			<div className="lg:col-start-2 lg:col-end-5">
-				<YearPage />
+				<Login />
 			</div>
 		</div>
-	);
+	); 
 }
-
+ 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,39 @@
+const Login = () => {
+	return (
+		<div className="text-white px-4 py-8">
+			<h1 className="text-2xl font-primary tracking-">
+				Welcome to the <br />{" "}
+				<span className="text-4xl font-extrabold">Year Book</span>
+			</h1>
+			<form className="flex flex-col my-8 gap-6">
+				<label
+					htmlFor="username"
+					className="flex flex-col text-xl">
+					Username
+					<input
+						className="outline-none h-12 rounded text-black px-2 mb-2"
+						type="text"
+						name="username"
+						id="username"
+					/>
+				</label>
+				<label
+					htmlFor="password"
+					className="flex flex-col text-xl">
+					Password
+					<input
+						className="outline-none h-12 rounded text-black px-2 mb-2"
+						type="password"
+						name="password"
+						id="password"
+					/>
+				</label>
+				<button className="w-full h-14 bg-gray-800 text-xl font-primary rounded mt-2">
+					Login
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default Login;
